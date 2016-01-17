4ed60f37e70446dd08ed0da4e75ce9b640b972e6
Madjarov <yusm@abv.bg>
2016-01-17 23:22:27 +0200

    add css  & JavaScript dir

--- application/views/header.php
+++ application/views/header.php
@@ -5,6 +5,7 @@
  * Date: 10.1.2016 г.
  * Time: 0:57
  */
+
 ?>
 
 <!DOCTYPE html>
@@ -12,7 +13,8 @@
     <meta charset="UTF-8">
 
     <title>Students manager</title>
-    <link rel="stylesheet" type="text/css" href="" media=""/>
+    <link rel="stylesheet" type="text/css" href="<?php echo BASE_PATH ?>/public/css/app.css"/>
+    <script type="text/javascript" src="<?php echo BASE_PATH ?>/public/js/app.js"></script>
 </head>
 
 <body>
---
+++ public/js/app.js
@@ -0,0 +1,3 @@
+/**
+ * Created by mrmad on 17.1.2016 г..
+ */
