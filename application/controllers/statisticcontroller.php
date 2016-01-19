c3b18578eb3ab639a2eb4ce4059cd0e4663a3647
Madjarov <yusm@abv.bg>
2016-01-20 00:54:08 +0200

    add best last  hour, day, week, month, class of

--- application/controllers/statisticcontroller.php
+++ application/controllers/statisticcontroller.php
@@ -8,52 +8,160 @@
  */
 class StatisticController extends Controller
 {
-    function  index(){
-
-
+    function index()
+    {
+        $model = new Statistic();
+        $selectAlumni = '';
+        $alumni = $model->query("SELECT DISTINCT alumni FROM  students");
+        foreach ($alumni as $key => $value) {
+            foreach ($value as $k => $v)
+                $selectAlumni .= '<option value=' . $v['alumni'] . "> " . $v['alumni'] . "</option>";
+        }
+        $this->set('selectAlumni', $selectAlumni);
     }
-    function bestNow(){
+
+    function bestNow()
+    {
         $this->doNotRenderHeader = 1;
         $st_activity = new Activities_student();
 
-       $students =  $st_activity->query("SELECT  first_name,last_name, f_number,  sum(point) point  FROM activities_students
+        $students = $st_activity->query("SELECT  first_name,last_name, f_number, `group`,  sum(point) point  FROM activities_students
 LEFT JOIN activitys   ON activities_students.activity_id = activitys.id
 LEFT JOIN students   ON activities_students.student_id = students.id
-GROUP BY student_id");
+GROUP BY student_id
+ORDER BY point DESC");
 
         $result = array();
-        foreach($students as $key => $student){
-            $st =  $student['Student']['first_name'] ." " .$student['Student']['last_name'];
-            $pt =  $student['']['point'];
-            array_push($result,array($st => $pt));
+        foreach ($students as $key => $student) {
+            $st = $student['Student']['first_name'] . " " . $student['Student']['last_name'];
+            $pt = $student['']['point'];
+            array_push($result, array($st => $pt));
         }
         $best = max($result);
-        $key = array_keys( $best );
+        $key = array_keys($best);
         $points = $best[$key[0]];
-
-        echo  $key[0]. " : ". $points. " points.";
-        die();
+        echo "<br>";
+        echo $key[0] . " - " . $points . " points.";
     }
 
-    function bestLastHour(){
-
+    function bestLastHour()
+    {
+        $this->doNotRenderHeader = 1;
+        $this->bestForLast("HOUR");
     }
-    function bestLastDay(){
 
+    function bestLastDay()
+    {
+        $this->doNotRenderHeader = 1;
+        $this->bestForLast("DAY");
     }
-    function bestLastWeek(){
 
+    function bestLastWeek()
+    {
+        $this->doNotRenderHeader = 1;
+        $this->bestForLast("WEEK");
     }
-    function bestLastMonth(){
 
+    function bestLastMonth()
+    {
+        $this->doNotRenderHeader = 1;
+        $this->bestForLast("MONTH");
     }
-    function bestInGroup(){
 
+    function bestInGroup($id)
+    {
+        $this->doNotRenderHeader = 1;
+        $this->bestIn('group', $id);
     }
-    function bestInFlow(){
 
+    function bestInFlow($id)
+    {
+        $this->doNotRenderHeader = 1;
+        $this->bestIn('flow', $id);
     }
-    function bestAlumni(){
+
+    function bestInAlumni($id)
+    {
+        $this->doNotRenderHeader = 1;
+        $this->bestIn('alumni', $id);
+    }
+
+    /**
+     * @param $string
+     */
+    private function bestForLast($string)
+    {
+        $st_activity = new Activities_student();
+
+        $students = $st_activity->query("SELECT  first_name,last_name, f_number,  sum(point) point  FROM activities_students
+  LEFT JOIN activitys   ON activities_students.activity_id = activitys.id
+  LEFT JOIN students   ON activities_students.student_id = students.id
+WHERE created_at > DATE_SUB(NOW(), INTERVAL 1 " . "$string" . ")
+GROUP BY student_id
+ORDER BY point DESC");
+        $result = array();
+        $i = 0;
+        foreach ($students as $key => $student) {
+            $st = $student['Student']['first_name'] . " " . $student['Student']['last_name'];
+            $pt = $student['']['point'];
+            array_push($result, array($st => $pt));
+        }
+        if (!empty($result)) {
+            foreach ($result as $name => $point) {
+                foreach ($point as $key => $value) {
+                    echo "<br>";
+                    echo ++$i . ". " . $key . " " . $value;
+                    echo "<br>";
+                }
+
+            }
+        } else {
+            echo "<br>No active students last " . strtolower($string) . "!";
+        }
+    }
+
+    /**
+     * @param $section
+     * @param $num
+     * @param string $string
+     */
+    private function bestIn($section, $num, $string = "YEAR")
+    {
+        $st_activity = new Activities_student();
+
+        $students = $st_activity->query("SELECT  first_name,last_name, f_number,  sum(point) point  FROM activities_students
+  LEFT JOIN activitys   ON activities_students.activity_id = activitys.id
+  LEFT JOIN students   ON activities_students.student_id = students.id
+WHERE created_at > DATE_SUB(NOW(), INTERVAL 1 " . "$string" . ") AND  `$section` = $num
+GROUP BY student_id
+ORDER BY point DESC");
+        $result = array();
+        $i = 0;
+        foreach ($students as $key => $student) {
+            $st = $student['Student']['first_name'] . " " . $student['Student']['last_name'];
+            $pt = $student['']['point'];
+            array_push($result, array($st => $pt));
+        }
+        if (!empty($result)) {
+            foreach ($result as $name => $point) {
+                foreach ($point as $key => $value) {
+                    echo "<br>";
+                    echo ++$i . ". " . $key . " - " . $value . " points.";
+                    echo "<br>";
+                }
+
+            }
+        } else {
+            echo "<br>No active students!";
+        }
+    }
+
+    private function studentInfo($section, $flow, $alumni, $subject)
+    {
+        $model = new Student();
+
+        $info = $model->query("SELECT DISTINCT `$section` FROM students
+WHERE flow = $flow AND  alumni = $alumni AND students.subject = '" . $subject . "'");
 
     }
 }
--- application/views/statistic/index.php
+++ application/views/statistic/index.php
@@ -7,27 +7,42 @@
  */
 ?>
 <script>
-function bestNow() {
+    function best() {
 
-    if (window.XMLHttpRequest) {
-        // code for IE7+, Firefox, Chrome, Opera, Safari
-        xmlhttp=new XMLHttpRequest();
-    } else { // code for IE6, IE5
-        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-    }
-    xmlhttp.onreadystatechange=function() {
-        if (xmlhttp.readyState==4 && xmlhttp.status==200) {
-
-            document.getElementById("txtHint").innerHTML=xmlhttp.responseText;
+        if (window.XMLHttpRequest) {
+            // code for IE7+, Firefox, Chrome, Opera, Safari
+            xmlhttp = new XMLHttpRequest();
+        } else { // code for IE6, IE5
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
         }
+        xmlhttp.onreadystatechange = function () {
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+
+                document.getElementById("txtHint").innerHTML = xmlhttp.responseText;
+            }
+        }
+        var action = arguments[0];
+        var e = document.getElementById("alumni");
+        var str = e.value;
+
+        xmlhttp.open("POST", "index.php?url=statistic/best" + action + "/" + str, true);
+        xmlhttp.send();
+
     }
 
-  xmlhttp.open("POST","index.php?url=statistic/bestNow",true);
-  xmlhttp.send();
-
-}
 </script>
 
-<button onclick=bestNow()>Best now</button>
+<button onclick=best("now")>Best of all</button>
+<button onclick=best("lasthour")>Best last hour</button>
+<button onclick=best("lastday")>Best last day</button>
+<button onclick=best("lastweek")>Best last week</button>
+<button onclick=best("lastmonth")>Best last month</button>
+
+
+Best in class of: <?php echo "<select id='alumni' onchange=best('inalumni') name='id'>
+                            <option>--Select class -- </option>";
+                                echo $selectAlumni;
+                                echo "</select>"; ?><br>
+
 <br>
-<div id="txtHint"><b>Best student now</b></div>
+<div id="txtHint"><b>Click on buttons for results.</b></div>
